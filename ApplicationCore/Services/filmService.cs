8e367f22ca81e17cc85d9f325bb2fd9aee718c06
zackdarklord <zakaria.chelbi@esprit.tn>
2023-05-12 15:10:38 +0100

    base generée

--- ApplicationCore/Services/filmService.cs
+++ ApplicationCore/Services/filmService.cs
@@ -2,13 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ApplicationCore.Services
 {
-    public class filmService:Service<Film>
+    public class filmService
     {
-
+        public Service<Film> service;
+/*
+        public  IEnumerable<Film> GetFilmPlusVus()
+        {
+            return service.GetMany();
+        }
+*/
     }
 }
